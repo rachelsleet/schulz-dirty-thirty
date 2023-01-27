5bf17eadba729865982c8a7bc1e1dc6b023aa893
Rachel Sleet <rachelsleet@gmail.com>
2023-01-27 22:57:31 +0100

    Add scrollable tabs on mobile

--- src/BasicTabs.js
+++ src/BasicTabs.js
@@ -87,7 +87,7 @@ export default function BasicTabs() {
   return (
     <Box sx={{ width: '100%' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example">
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" variant="scrollable" allowScrollButtonsMobile>
           <Tab label="Wilkommen!" {...a11yProps(0)} />
           <Tab label="Ladies Night" {...a11yProps(1)} />
           <Tab label="Life Advice" {...a11yProps(2)} />
